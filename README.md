933c756d5daae8b0ac8539e66b1a4c14a5e2f6ff
8BitGinger <ryan.fann@gmail.com>
2023-09-05 14:32:46 -0500

    Erased everything and started with new tutorial.  First functional one.  Still have more to fix

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # challenge3
+
+Links I used for help:
+For the Generate of Random Passwords I used:
+
+https://www.youtube.com/watch?v=duNmhKgtcsI&t=129s
+
+https://codepen.io/FlorinPop17/pen/BaBePej
+
+I watched a few dozen tutorials before settling on the last link.  This guy explained it well, but I still couldn't figure out how to do it exactly correctly.  I also skipped the clipboard button.  I tried for 20 hours to build it with many tutorials and this was the only one that made some sense.  
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,71 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//These are our constants or variables.  This assigns the ID to a variable we can use in JS
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
 
-  passwordText.value = password;
-
+//this puts all of our get functions into one functions we can use later in our loop.
+const randomFunc = {
+	lower: getRandomLower,
+	upper: getRandomUpper,
+	number: getRandomNumber,
+	symbol: getRandomSymbol
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+// this listens for us to click the button Generate with ID.  Then it will check the value of length, and the checkboxes and return a generated password
+generate.addEventListener('click', () => {
+	const length = +lengthEl.value;
+	const hasLower = lowercaseEl.checked;
+	const hasUpper = uppercaseEl.checked;
+	const hasNumber = numbersEl.checked;
+	const hasSymbol = symbolsEl.checked;
+	
+	resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+
+//This function generates the password
+function generatePassword(lower, upper, number, symbol, length) {
+	let generatedPassword = '';
+	const typesCount = lower + upper + number + symbol;
+	const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+	
+
+	if(typesCount === 0) {
+		return '';
+	}
+	
+	// we use this to create a loop for each array and then return a final password
+	for(let i=0; i<length; i+=typesCount) {
+		typesArr.forEach(type => {
+			const funcName = Object.keys(type)[0];
+			generatedPassword += randomFunc[funcName]();
+		});
+	}
+	
+	const finalPassword = generatedPassword.slice(0, length);
+	
+	return finalPassword;
+}
+
+// these functions return the strings we need to create the password.
+function getRandomLower() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+	const symbols = '!@#$%^&*(){}[]=<>/,.'
+	return symbols[Math.floor(Math.random() * symbols.length)];
+}
